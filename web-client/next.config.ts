97443b199b782a1ccba72314e1b014d87c7cebce
luthfiupb5 <luthfimonu1@gmail.com>
2026-01-09 02:57:25 +0530

    Fix: Increase Payload Size Limit to 10MB

--- web-client/next.config.ts
+++ web-client/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '10mb',
+    },
+  },
+  images: {
+    domains: ['scbqioxxfyoghyeawhfz.supabase.co'],
+  },
 };
 
 export default nextConfig;
